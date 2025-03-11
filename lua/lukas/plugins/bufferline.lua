5b1999c41f36bdc34df9bbf84e5292d58c00d4d6
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-11 09:32:37 +0100

    bufferline: changed from tabs to buffers.

--- lua/lukas/plugins/bufferline.lua
+++ lua/lukas/plugins/bufferline.lua
@@ -1,11 +1,11 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  versions = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	versions = "*",
+	opts = {
+		options = {
+			mode = "buffers",
+			separator_style = "slant",
+		},
+	},
 }
