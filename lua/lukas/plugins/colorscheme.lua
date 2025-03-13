6f5326e657f867d63888207b657b0820454a99cd
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-13 06:37:56 +0100

    colorscheme: added more themes.

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 return {
-	{
+	{ -- https://github.com/catppuccin/nvim
 		"catppuccin/nvim",
 		priority = 1000,
 		name = "catppuccin",
@@ -46,4 +46,9 @@ return {
 			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
+	{ "EdenEast/nightfox.nvim" }, -- https://github.com/EdenEast/nightfox.nvim
+	{ "olimorris/onedarkpro.nvim" }, -- https://github.com/olimorris/onedarkpro.nvim
+	{ "Shatur/neovim-ayu" }, -- https://github.com/Shatur/neovim-ayu
+	{ "rakr/vim-one" }, -- https://github.com/rakr/vim-one
+	{ "NLKNguyen/papercolor-theme" }, -- https://github.com/NLKNguyen/papercolor-theme
 }
