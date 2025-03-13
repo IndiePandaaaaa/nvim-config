7211d33d494d4548ee8be16b896d8eefb9c8489b
IndiePandaaaaa|Lukas <indiepandaaaaa@gmx.net>
2025-03-13 05:46:22 +0100

    colorscheme: fixed colorscheme.

--- lua/lukas/plugins/colorscheme.lua
+++ lua/lukas/plugins/colorscheme.lua
@@ -1,2 +1,49 @@
-require("catppuccin")
-vim.cmd.colorscheme("catppuccin")
+return {
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "auto",
+				background = { light = "latte", dark = "macchiato" },
+				transparent_background = false,
+				show_end_of_buffer = true,
+				term_colors = false,
+				dim_inactive = { enabled = false, shade = "dark", percentage = 0.15 },
+				no_italic = true,
+				no_bold = false,
+				no_underline = false,
+				color_overrides = {},
+				default_integrations = true,
+				styles = {
+					comments = {},
+					conditionals = {},
+					loops = {},
+					functions = { "bold" },
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				integrations = {
+					alpha = true,
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					rainbow_delimiters = true,
+					telescope = { enabled = true, style = "nvchad" },
+					treesitter = true,
+					notify = true,
+					mini = { enabled = true, indentscope_color = "" },
+					which_key = true,
+				},
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
+}
